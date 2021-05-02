ed836a777fbbed33240e981f281526bf83688350
Huaixi Lu <huaixil@princeton.edu>
2021-05-02 18:09:53 -0400

    HLIV added

--- HLIV/hliv.tcl
+++ HLIV/hliv.tcl
@@ -7,9 +7,9 @@ elaborate -top bsg_top
 clock base_clk;
 clock base_clk io_clk 2 1 -both_edges;
 clock base_clk core_clk 4 1
-clock base_clk edge_clk_i 4 2;
+clock base_clk edge_clk_i 4 4;
 reset -expression {rst} {:global_formal_reset} -non_resettable_regs {0};
 
 assume {~core_yumi_i || valid_o}
-assert {##10 edge_clk == edge_clk_i[0]}
+assert {##10 edge_clk[0] == edge_clk_i}
 assert {@(posedge core_clk) valid_i |=> ##[0:$] valid_o}
